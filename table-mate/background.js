011eca9f95727d48b4e7adbc6e98e42b0b2c4937
ohnas <ohnas12@gmail.com>
2025-03-25 15:05:46 +0900

    tabs permissions add

--- table-mate/background.js
+++ table-mate/background.js
@@ -2,8 +2,16 @@ let extensionActive = false;
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "startExtension") {
+        console.log("[TableMate] Start message received");
         extensionActive = true;
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            console.log("[TableMate] tabs 결과:", tabs);
+            const tab = tabs[0];
+            if (!tab) {
+                console.error("[TableMate] 활성 탭 정보를 찾을 수 없습니다.");
+                return;
+            }
+            console.log("[TableMate] Target tab ID:", tab.id);
             chrome.scripting.executeScript({
                 target: { tabId: tabs[0].id },
                 function: activateSelectionMode
@@ -11,8 +19,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         });
     } 
     else if (message.action === "stopExtension") {
+        console.log("[TableMate] Stop message received");
         extensionActive = false;
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            console.log("[TableMate] tabs 결과:", tabs);
             chrome.scripting.executeScript({
                 target: { tabId: tabs[0].id },
                 function: deactivateSelectionMode
